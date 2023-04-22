cbd2b48cf1aca446aee87d55236b546d961342ba
Gunveen Kaur <sainiaryan2222@gmail.com>
2023-04-22 13:24:00 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+  env: {
+    API_KEY: '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',
+  },
+};
